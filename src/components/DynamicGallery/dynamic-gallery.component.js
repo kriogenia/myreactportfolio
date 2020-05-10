d8d90853364ba1c8a891197e373189a882c32b70
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-05-10 21:15:55 +0200

    Finished the Interests page
    Close #41

--- src/components/DynamicGallery/dynamic-gallery.component.js
+++ src/components/DynamicGallery/dynamic-gallery.component.js
@@ -2,41 +2,52 @@ import React from "react";
 import "./dynamic-gallery.css";
 import {useTranslation} from "react-i18next";
 
+/**
+ * @param name		Codename to load the translations
+ * @param images	Images to fill the gallery
+ * @returns {div}	Gallery with the info and 1-4 images in the background
+ */
 export const DynamicGalleryComponent = ({name, images}) => {
 
 	const [hovering, setHovering] = React.useState(false);	// Hover state
 
 	const {t} = useTranslation();
 
-	let  n = -1;
+	let  n = -1;														// Image counter
 
+	/**
+	 * @param n				Image counter
+	 * @returns {string}	Classes the image will receive based on the counter and the total of images
+	 */
 	const generateClasses = (n) => {
-		if (n === 0 && images.length === 1)	return "tl tr bl br expand";
-		if (n === 0 && images.length === 2)	return "tl bl";
-		if (n === 0)						return "tl";
-		if (n === 1 && images.length === 2)	return "tr br";
-		if (n === 1)						return "tr";
-		if (n === 2 && images.length === 3)	return "bl br expand";
-		if (n === 2)						return "bl";
-		if (n === 3)						return "br";
+		if (n === 0 && images.length === 1)	{return "tl tr bl br expand";}
+		if (n === 0 && images.length === 2)	{return "tl bl";}
+		if (n === 0)						{return "tl";}
+		if (n === 1 && images.length === 2)	{return "tr br";}
+		if (n === 1)						{return "tr";}
+		if (n === 2 && images.length === 3)	{return "bl br expand";}
+		if (n === 2)						{return "bl";}
+		if (n === 3)						{return "br";}
 	};
 
 	return (
 		<div className={"interest-container"} onMouseEnter={() => setHovering(true)}
-			 onMouseLeave={() => setHovering(false)}>
+				onMouseLeave={() => setHovering(false)}>
+			{/* Container with the title and description */}
 			<div className="interest-front">
 				<div className={"interest-info"}>
 					<h3>{t(`interests:${name}.name`)}</h3>
 					{hovering? <p>{t(`interests:${name}.description`)}</p> : null}
 				</div>
 			</div>
+			{/* Container with the background images */}
 			<div className="interest-back">
 				{ images.map((i) => {
 					let classes = generateClasses(++n);
 					return (
 						<div className={`interest-figure ${classes} ${hovering? "showName" : ""}`}
 							style={{backgroundImage: `url(${process.env.PUBLIC_URL + i.src})`}}>
-							{i.name}
+							{t(`interests:${i.name}`)}
 						</div>
 					);})}
 			</div>
--- src/i18n.js
+++ src/i18n.js
@@ -9,7 +9,7 @@ i18n
 	.use(initReactI18next) 		// passes i18n down to react-i18next
 	.init({
 		fallbackLng: "en", 		// use en if detected lng is not available
-		ns: ["translation", "bio", "portfolio", "skills", "cv"],
+		ns: ["translation", "bio", "portfolio", "skills", "cv", "interests"],
 		debug: true,
 		saveMissing: true,
 		backend: {
