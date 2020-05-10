ff6858e8d15c4eba58bd2972a5ced9346dd53af0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-10 21:35:14 +0000

    Deploy kriogenia/sotoestevezdotric to github.com/kriogenia/sotoestevezdotric.git:gh-pages

---
+++ precache-manifest.fba7df5390d6393df0fbaa1418da019b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8741c774a0a9db5592dabb9c4c5194e3",
+    "url": "/myreactportfolio/index.html"
+  },
+  {
+    "revision": "a82e96d3ff198ea09442",
+    "url": "/myreactportfolio/static/css/main.03557ccc.chunk.css"
+  },
+  {
+    "revision": "9e644fb8c1c4276dc9f7",
+    "url": "/myreactportfolio/static/js/2.3780fe0c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/myreactportfolio/static/js/2.3780fe0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82e96d3ff198ea09442",
+    "url": "/myreactportfolio/static/js/main.85a109de.chunk.js"
+  },
+  {
+    "revision": "f4043734dad607958303",
+    "url": "/myreactportfolio/static/js/runtime-main.cfd7c058.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "/myreactportfolio/static/media/getFetch.5e98861f.cjs"
+  },
+  {
+    "revision": "3b995a3e1798e7462141320e1d10f31a",
+    "url": "/myreactportfolio/static/media/me.3b995a3e.png"
+  }
+]);
